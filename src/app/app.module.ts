3cf0c963ce1f1637e13de3f7a4ed66845d2c4349
suwigyarathore <Suwi.Rathore@klm.com>
2018-06-10 00:23:49 +0200

    Adding lib & component to main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng6LibModule } from 'ng6-lib';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng6LibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
